b78c4891f0bad429f8c0366901dd912729b92505
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-09-08 06:09:58 -0300

    welcome and app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Welcome } from "./components/Welcome";
+
+import "./App.css";
 
 export const App = () => {
   return (
     <div id="app">
       <h1>Quiz de programação</h1>
-      <Welcome/>
+      <Welcome />
     </div>
   );
 };
