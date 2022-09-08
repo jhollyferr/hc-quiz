e4950178e4960ff2c1cc2e50522d767410ca561f
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-09-08 06:19:10 -0300

    Created Quiz Context

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { QuizContext } from "../../context/quiz";
 import ImageQuiz from "../../img/quiz.svg";
 
 import "./index.css";
 
 export const Welcome = () => {
+  const quizState = useContext(QuizContext);
+  
   return (
     <div id="welcome">
       <h1>Seja bem-vindo</h1>
